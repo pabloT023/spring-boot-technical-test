adae9f835b0dc143a149a2f691af18c318a799da
Pablo Tezó <pablo@MacBook-Air-de-Pablo.local>
2025-02-09 21:58:21 -0600

    First commit

---
+++ src/main/java/com/bank/spring_boot_technical_test/SpringBootTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.bank.spring_boot_technical_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTechnicalTestApplication.class, args);
+	}
+
+}
